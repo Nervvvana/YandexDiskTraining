520f7164ce4987a830f01ffb1eb017428df84cb3
nervvvana <mrnenot23@gmail.com>
2026-03-18 15:43:23 +0300

    Настройка конфигурации

--- src/test/java/api/v1/disk/config/TestConfig.java
+++ src/test/java/api/v1/disk/config/TestConfig.java
@@ -1,30 +1,51 @@
 package api.v1.disk.config;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public final class TestConfig {
+
+    private static final Properties props = new Properties();
+
+    static {
+        try (InputStream is = TestConfig.class
+                .getClassLoader()
+                .getResourceAsStream("application.properties")) {
+
+            if (is != null) {
+                props.load(is);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка загрузки файла application.properties", e);
+        }
+    }
 
     private TestConfig() {}
 
-    private static String getProperty(String key, String defaultValue) {
-
+    private static String get(String key, String defaultValue) {
         String value = System.getProperty(key);
-
-        if (value == null || value.isBlank()) {
-            value = System.getenv(key);
+        if (value != null && !value.isBlank()) {
+            return value;
         }
 
-        return value != null ? value : defaultValue;
+        value = props.getProperty(key);
+        if (value != null && !value.isBlank()) {
+            return value;
+        }
+
+        return defaultValue;
     }
 
     public static final String BASE_URI =
-            getProperty("BASE_URL", "https://cloud-api.yandex.net");
+            get("base.url", "https://storage.net");
 
     public static final String BASE_PATH =
-            getProperty("BASE_PATH", "/v1/disk");
-
-    public static final String OAUTH_TOKEN =
-            getProperty("OAUTH_TOKEN", "");
+            get("base.path", "/v1/disk");
 
     public static final String DISK_TOTAL_SPACE =
-            getProperty("DISK_TOTAL_SPACE", "5368709120");
+            get("disk.total.space", "5368709120");
 
+    public static final String OAUTH_TOKEN =
+            get("oauth.token", null);
 }
